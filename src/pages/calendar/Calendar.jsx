25280cf571e61a2c1aa3dd42c2ac6c448f8fffe9
전경수 <wjsrudtn9338@genexon.co.kr>
2026-05-06 15:32:51 +0900

    [기타] 캘린더 초기 완료.

--- src/pages/calendar/Calendar.jsx
+++ src/pages/calendar/Calendar.jsx
@@ -4,7 +4,19 @@ import dayGridPlugin from '@fullcalendar/daygrid';
 import interactionPlugin from '@fullcalendar/interaction';
 import {useEffect, useState} from "react";
 import api from "../../api/api.js";
-import {Badge, Button, Card, Group, Input, Modal, SegmentedControl, Stack, Text, TextInput} from "@mantine/core";
+import {
+    Badge,
+    Button,
+    Card,
+    Divider,
+    Group,
+    Input,
+    Modal,
+    SegmentedControl,
+    Stack,
+    Text,
+    TextInput
+} from "@mantine/core";
 
 export default function Calendar() {
     const [workoutEvents, setWorkoutEvents] = useState([])
@@ -13,13 +25,11 @@ export default function Calendar() {
 
         const res = await api.get("/calendar/workout/list");
 
-        console.log(res);
-
         const workoutEvents = res.data.map((item) => ({
-            id: item.scheduleId,
+            id: String(item.scheduleId),
             title: item.scheduleName,
             date: item.scheduleDate,
-            className: item.scheduleType,
+            className: item.scheduleStatus,
             extendedProps: {
                 templateId: item.templateId,
                 scheduleStatus: item.scheduleStatus,
@@ -37,6 +47,7 @@ export default function Calendar() {
     return (
         <FullCalendarUI
             workoutEvents={workoutEvents}
+            refreshEvents={getWorkoutEvents}
         />
     )
 }
@@ -88,7 +99,8 @@ const RoutineCardList = ({setTitle, setTemplateId}) => {
     )
 }
 
-const CustomScheduleForm = ({setTitle}) => {
+const CustomScheduleForm = ({setTitle, setTemplateId}) => {
+    setTemplateId(null);
     return (
         <div className={classes.customForm}>
             <TextInput
@@ -102,7 +114,7 @@ const CustomScheduleForm = ({setTitle}) => {
     )
 }
 
-const DateModal = ({opened, onClose, selectedDate}) => {
+const DateModal = ({opened, onClose, selectedDate, refreshEvents}) => {
     const [scheduleMode, setScheduleMode] = useState("ROUTINE");
     const [status, setStatus] = useState("PLANNED");
     const [title, setTitle] = useState("");
@@ -112,9 +124,18 @@ const DateModal = ({opened, onClose, selectedDate}) => {
     const handleClose = () => {
         onClose(false);
         setScheduleMode("ROUTINE");
+        setTemplateId(null);
+        setTitle("");
+        setMemo("");
+        setStatus("PLANNED");
     }
 
     async function saveWorkout() {
+
+        if(title === "") {
+            alert("제목을 고르시거나 작성해주세요!");
+            return;
+        }
 
         const params = {
             title: title,
@@ -123,10 +144,11 @@ const DateModal = ({opened, onClose, selectedDate}) => {
             memo: memo,
             status: status,
         }
-
         const res = await api.post("/calendar/workout/save", params);
 
-        console.log(res);
+        handleClose();
+        refreshEvents();
+        alert("저장 성공");
     }
 
     return(
@@ -156,6 +178,7 @@ const DateModal = ({opened, onClose, selectedDate}) => {
                         />}
                         {scheduleMode === "CUSTOM" && <CustomScheduleForm
                             setTitle={setTitle}
+                            setTemplateId={setTemplateId}
                         />}
                     </div>
 
@@ -163,7 +186,7 @@ const DateModal = ({opened, onClose, selectedDate}) => {
                         label="메모"
                         placeholder="오늘 운동에 대한 메모를 입력하세요"
                         radius="md"
-                        onChage={(e) => setMemo(e.target.value)}
+                        onChange={(e) => setMemo(e.target.value)}
                     />
 
                     <SegmentedControl
@@ -181,7 +204,7 @@ const DateModal = ({opened, onClose, selectedDate}) => {
                             취소
                         </Button>
 
-                        <Button onClick={() => console.log(title)}>
+                        <Button onClick={() => saveWorkout()}>
                             저장하기
                         </Button>
                     </Group>
@@ -201,10 +224,11 @@ const formatDate = (dateStr) => {
     });
 }
 
-const FullCalendarUI = ({workoutEvents}) => {
+const FullCalendarUI = ({workoutEvents, refreshEvents}) => {
     const [selectedDate, setSelectedDate] = useState(null);
     const [dateOpened, setDateOpened] = useState(false);
     const [exOpen, setExOpen] = useState(false);
+    const [detail, setDetail] = useState(null);
 
     return (
         <>
@@ -231,12 +255,13 @@ const FullCalendarUI = ({workoutEvents}) => {
                         buttonText={{
                             today: "오늘",
                         }}
-                        dayMaxEvents={3}
+                        dayMaxEvents={5}
                         dateClick={(info) => {
                             setSelectedDate(info.dateStr);
                             setDateOpened(true);
                         }}
                         eventClick={(info) => {
+                            setDetail(info.event);
                             setExOpen(true);
                         }}
                     />
@@ -247,7 +272,125 @@ const FullCalendarUI = ({workoutEvents}) => {
                 opened={dateOpened}
                 onClose={() => setDateOpened(false)}
                 selectedDate={selectedDate}
+                refreshEvents={refreshEvents}
             />
+
+            <WorkoutDetailModal
+                opened={exOpen}
+                onClose={() => setExOpen(false)}
+                detail={detail}
+                refreshEvents={refreshEvents}
+            />
+        </>
+    )
+}
+
+const WorkoutDetailModal = ({opened, onClose, detail, refreshEvents}) => {
+    const [detailMemo, setDetailMemo] = useState("");
+    const [detailStatus, setDetailStatus] = useState(detail?.extendedProps?.scheduleStatus || "PLANNED");
+    const [detailTitle, setDetailTitle] = useState("");
+
+    useEffect(() => {
+        setDetailMemo(detail?.extendedProps?.memo || "");
+        setDetailTitle(detail?.title || "");
+        setDetailStatus(detail?.extendedProps?.scheduleStatus || "PLANNED");
+    },[detail])
+
+    if (!detail) return null;
+    const scheduleId = detail.id;
+
+     async function deleteWorkoutDetail() {
+         await api.delete(`/calendar/workout/delete/${scheduleId}`);
+         alert("삭제를 완료했습니다.");
+         onClose();
+         refreshEvents();
+     }
+
+     async function updateWorkoutDetail() {
+         const params = {
+             scheduleId: scheduleId,
+             title: detailTitle,
+             memo: detailMemo,
+             status: detailStatus,
+         }
+
+         const res = await api.put("/calendar/workout/update", params);
+
+         if(res.status === 200){
+             refreshEvents();
+         }
+     }
+
+    return(
+        <>
+            <Modal
+                opened={opened}
+                onClose={onClose}
+                centered
+                size="md"
+                title="운동 상세"
+            >
+                <Stack gap="md">
+                    <Group justify="space-between" align="flex-start">
+                        <div>
+                            <Text size="xs" c="dimmed" fw={700}>
+                                운동 일정
+                            </Text>
+
+
+                            <TextInput
+                                variant="unstyled"
+                                value={detailTitle}
+                                onChange={(e) => setDetailTitle(e.target.value)}
+                                classNames={{
+                                    input: classes.detailTitleInput,
+                                }}
+                            />
+
+                        </div>
+
+                        <Badge
+                            size="lg"
+                            radius="md"
+                            color={detailStatus === "FINISHED" ? "green" : "blue"}
+                            variant="light"
+                            style={{ cursor: "pointer" }}
+                            onClick={() => {
+                                setDetailStatus((prev) => prev === "PLANNED" ? "FINISHED" : "PLANNED")
+                            }}
+                        >
+                            {detailStatus === "FINISHED" ? "완료" : "예정"}
+                        </Badge>
+                    </Group>
+
+                    <Divider />
+
+                    <Card withBorder radius="lg" padding="md" className={classes.detailMemoCard}>
+                        <Text size="sm" c="dimmed" fw={700} mb={6}>
+                            메모
+                        </Text>
+
+                        <TextInput
+                            variant="unstyled"
+                            value={detailMemo}
+                            onChange={(e) => setDetailMemo(e.target.value)}
+                            className={{
+                                input: classes.detailMemoInput
+                            }}
+                        />
+                    </Card>
+
+                    <Group justify="flex-end" mt="sm">
+                        <Button variant="light" color="gray" onClick={() => deleteWorkoutDetail()}>
+                            삭제
+                        </Button>
+
+                        <Button onClick={() => updateWorkoutDetail()}>
+                            수정하기
+                        </Button>
+                    </Group>
+                </Stack>
+            </Modal>
         </>
     )
 }
